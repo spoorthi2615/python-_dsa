9c12875a8d21e19c69e4a3cd9d3f8ad2d77246cb
spoorthi2615 <spoorthipyadav@gmail.com>
2026-06-03 12:57:05 +0530

    string- leetcode question

--- DAY 5/strings.py
+++ DAY 5/strings.py
@@ -102,3 +102,20 @@ def reverse_vowels(s):
 print(reverse_vowels("Neha__"))
 
 #***************************************************************************************************
+
+#Reverse Only Letters
+def reverse_only_letters(s):
+    s = list(s)
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if not s[left].isalpha():
+            left += 1
+        elif not s[right].isalpha():
+            right -= 1
+        else:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+    return "".join(s)
