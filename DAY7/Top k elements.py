00ed5c5d5ed78f745c9e9f53f998ed188ead0655
spoorthi2615 <spoorthipyadav@gmail.com>
2026-06-05 10:01:26 +0530

    added new problem

---
+++ DAY7/Top k elements.py
@@ -0,0 +1,20 @@
+#top k elements
+#Given an integer array nums and an integer k, return the k most frequent elements.
+#You may return the answer in any order.
+class solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        freq = [[] for i in range(len(nums) + 1)]
+        for num, c in count.items():
+            freq[c].append(num)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
+
+ 
---
+++ DAY7/backsapce string.py
@@ -0,0 +1,14 @@
+#Given two strings s and t, return true if they are equal when both are typed into empty text editors.
+#  '#' means a backspace character.
+# Note that after backspacing an empty text, the text will continue empty.
+def backspaceCompare(s: str, t: str) -> bool:
+    def build(string):
+        result = []
+        for char in string:
+            if char != '#':
+                result.append(char)
+            elif result:
+                result.pop()
+        return ''.join(result)
+    
+    return build(s) == build(t)
