f5ead110ee573061572fe562974afa5ff2a65c3a
spoorthi2615 <spoorthipyadav@gmail.com>
2026-06-06 09:18:04 +0530

    leetcode problems

---
+++ DAY 8/Maximum Nesting Depth of the Parentheses.py
@@ -0,0 +1,15 @@
+#Given a valid parentheses string s, return the nesting depth of s. 
+# The nesting depth is the maximum number of nested parentheses.
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        stack = []
+        max_depth = 0
+        
+        for char in s:
+            if char == '(':
+                stack.append(char)
+                max_depth = max(max_depth, len(stack))
+            elif char == ')':
+                stack.pop()
+        
+        return max_depth
--- DAY 8/clear digits.py
+++ DAY 8/clear digits.py
@@ -3,4 +3,14 @@
 # Delete the first digit and the closest non-digit character to its left.
 # Return the resulting string after removing all digits.
 # Note that the operation cannot be performed on a digit that does not have any non-digit character to its left.
-def 
+def clearDigits(s):
+    stack = []
+    
+    for char in s:
+        if char.isdigit():
+            if stack:
+                stack.pop()
+        else:
+            stack.append(char)
+    
+    return ''.join(stack)
