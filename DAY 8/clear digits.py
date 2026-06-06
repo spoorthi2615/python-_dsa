cde11245bc68fcfd8bab42d27b13e88b65b045b2
spoorthi2615 <spoorthipyadav@gmail.com>
2026-06-06 09:09:31 +0530

    few edits

---
+++ DAY 8/clear digits.py
@@ -0,0 +1,6 @@
+#You are given a string s.
+# Your task is to remove all digits by doing this operation repeatedly:
+# Delete the first digit and the closest non-digit character to its left.
+# Return the resulting string after removing all digits.
+# Note that the operation cannot be performed on a digit that does not have any non-digit character to its left.
+def 
