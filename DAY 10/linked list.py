5db7a63bc25f46b79659bf3d7a34bcbf70f88f3f
spoorthi2615 <spoorthipyadav@gmail.com>
2026-06-09 10:30:29 +0530

    linked list

--- DAY 10/linked list.py
+++ DAY 10/linked list.py
@@ -14,6 +14,32 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+#insertion at begining in linked list
+    def insert_at_begining(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+#inerstion at end in linked list
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+#traversal in linked list
+    def traverse(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
 #creating a linked list
 linked_list = LinkedList()
 print(linked_list.head) #None
+
+
