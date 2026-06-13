97b7077b7a7532995eca8984befda8512d2f37d1
spoorthi2615 <spoorthipyadav@gmail.com>
2026-06-13 11:42:30 +0530

    new problems

--- DAY 14/Number of Islands.py
+++ DAY 14/Number of Islands.py
@@ -1,3 +1,37 @@
-# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands. 
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+#You may assume all four edges of the grid are all surrounded by water.
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
 
-# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+        rows = len(grid)
+        cols = len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            if (r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                grid[r][c] == '0'):
+                return
+
+            grid[r][c] = '0'
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    count += 1
+                    dfs(r, c)
+
+        return count
+        
